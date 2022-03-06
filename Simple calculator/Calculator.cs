f65e9f196837859b814f7731fd8aae110b101a7a
Neha Pardeshi <knitneha@gmail.com>
2022-03-06 20:07:37 +0100

    Added initial code

---
+++ Simple calculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator
+{
+	public class Calculator
+	{
+		public decimal Sum(decimal a, decimal b)
+		{
+			return a+b;
+		}
+		public decimal Difference(decimal a, decimal b)
+		{
+			return a-b;
+		}
+		public decimal Multiply(decimal a, decimal b)
+		{
+			return a*b;
+		}
+		public decimal Divide(decimal a, decimal b)
+		{
+			return a/b;
+		}
+		public decimal Mod(decimal a, decimal b)
+		{
+			return a%b;
+		}
+	}
+}
---
+++ Simple calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Calculator App");
+			var calcAction = "";
+			var calc = new Calculator();
+
+			do
+			{
+				Console.Write("Enter number 1: ");
+				decimal.TryParse(Console.ReadLine(),out decimal firstNumber);
+				Console.Write("Enter operator (+,-,*,/,%)");
+				var op = Console.ReadLine();
+				Console.Write("Enter number 2: ");
+				decimal.TryParse(Console.ReadLine(), out decimal secondNumber);
+
+				decimal result = 0;
+
+				switch (op)
+				{
+					case"+":
+					result = calc.Sum(firstNumber,secondNumber);
+						break;
+					case "-":
+						result = calc.Difference(firstNumber, secondNumber);
+						break;
+					case "*":
+						result = calc.Multiply(firstNumber, secondNumber);
+						break;
+					case "/":
+						result = calc.Divide(firstNumber, secondNumber);
+						break;
+					case "%":
+						result = calc.Mod(firstNumber, secondNumber);
+						break;
+
+					default:
+						Console.WriteLine("Not valid operation");
+						break;
+				}
+				Console.WriteLine($"{firstNumber} {op} {secondNumber} = {result}");
+				Console.WriteLine("");
+
+				Console.WriteLine("Enter 0 to exit. Enter any other key to continue...");
+				calcAction = Console.ReadLine();
+			} while (calcAction != "0");
+		}
+	}
+}
